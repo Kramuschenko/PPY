869eb7c3c8e888bd7a0adf7ebe90c02639eb963e
Kramuschenko <71378181+Kramuschenko@users.noreply.github.com>
2023-03-11 16:58:34 +0100

    homework 1

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,14 @@
 import webbrowser
+import requests
 
-url = input("Podaj adres strony: ")
-webbrowser.open(url)
+if __name__ == '__main__':
+    print("hello world")
+    url1 = input("Podaj stronę internetową: ")
+
+    for x in range(3):
+        date = input("podaj {} date: ".format(x+1))
+        url = "http://archive.org/wayback/available?url=" + url1 + "&timestamp=" + str(date)
+        response = requests.get(url)
+        d = response.json()
+        page = d["archived_snapshots"]["closest"]["url"]
+        webbrowser.open(page)
