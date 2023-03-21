cd2f867467e074ede2f52e9dc79134a12edbcbf0
Kramuschenko <71378181+Kramuschenko@users.noreply.github.com>
2023-03-21 09:26:24 +0100

    reformating answers

--- cwiczenia/2/task3.py
+++ cwiczenia/2/task3.py
@@ -1,71 +1,84 @@
+questions = []
+answers = []
+
 print("Witaj w ankiecie dotyczącej Twoich studiów!")
 imie_nazwisko = input("Jak masz na imię i nazwisko? ")
 
 print("Witaj {}! Odpowiedz na poniższe pytania:".format(imie_nazwisko))
-odpowiedz1 = input("1. Jaki jest twój ulubiony kolor? ")
-odpowiedz2 = input("2. Co robisz w wolnym czasie? ")
-odpowiedz3 = input("4. Co jest Twoim ulubionym daniem? ")
+kolor_ = "1. Jaki jest twój ulubiony kolor? "
+answers += [input(kolor_)]
+wolnyCzas = "2. Co robisz w wolnym czasie? "
+answers += [input(wolnyCzas)]
+jedzenie = "3. Co jest Twoim ulubionym daniem? "
+answers += [input(jedzenie)]
+questions += [kolor_]
+questions += [wolnyCzas]
+questions += [jedzenie]
 
-print("\n4. Kierunek studiów:")
+kierunek = "4. Kierunek studiów:"
+questions += [kierunek]
+print("%s" % kierunek)
 print("a) Informatyka")
 print("b) Zarządzanie")
 print("c) Media art")
-kierunek = input("Wybierz literę odpowiadającą Twojemu kierunkowi studiów: ")
+kierunek = input("Wybierz literę odpowiadającą Twojemu kierunkowi studiów lub jego nazwe: ")
 
-print("\n5. Stopień studiów:")
+if kierunek == "a":
+    answers += ["Informatyka"]
+elif kierunek == "b":
+    answers += ["Zarządzanie"]
+elif kierunek == "c":
+    answers += ["Media art"]
+else:
+    answers += [f"Inne: {kierunek}"]
+
+stopien = ["5. Stopień studiów:"]
+questions += stopien
+print(stopien)
 print("a) Studia inżynierskie")
 print("b) Studia magisterskie")
-stopien = input("Wybierz literę odpowiadającą Twojemu stopniowi studiów: ")
+stopien = input("Wybierz literę odpowiadającą Twojemu stopniowi studiów lub własną nazwe: ")
 
-print("\n6. Sposób finansowania studiów:")
+if stopien == "a":
+    answers += ["Studia inżynierskie"]
+elif stopien == "b":
+    answers += ["Studia magisterskie"]
+else:
+    answers += [f"Inne: {stopien}"]
+
+finansowanie = "6. Sposób finansowania studiów:"
+questions += [finansowanie]
+print(finansowanie)
 print("a) Studia zaoczne")
 print("b) Studia dzienne, płatne")
 finansowanie = input("Wybierz literę odpowiadającą sposobowi finansowania Twoich studiów: ")
 
-print("\n7. Planowana specjalizacja po ukończeniu studiów:")
+if finansowanie == "a":
+    answers += ["Studia zaoczne"]
+elif finansowanie == "b":
+    answers += ["Studia dzienne, płatne"]
+else:
+    answers += [f"Inne: {finansowanie}"]
+
+specjalizacja = "7. Planowana specjalizacja po ukończeniu studiów:"
+questions += [specjalizacja]
+print(specjalizacja)
 print("a) Programowanie aplikacji biznesowych")
 print("b) PM lub BA")
 print("c) 3D artist")
 specjalizacja = input("Wybierz literę odpowiadającą Twojej planowanej specjalizacji po ukończeniu studiów: ")
 
-
-print("\nPodsumowanie odpowiedzi:")
-print("1. Jaki jest twój ulubiony kolor? {}".format(odpowiedz1))
-print("2. Co robisz w wolnym czasie? {}".format(odpowiedz2))
-print("3. Co jest Twoim ulubionym daniem? {}".format(odpowiedz3))
-
-print("4. Kierunek studiów:", end=" ")
-if kierunek == "a":
-    print("Informatyka")
-elif kierunek == "b":
-    print("Zarządzanie")
-elif kierunek == "c":
-    print("Media art")
-else:
-    print("Inne: ", kierunek)
-
-print("5. Stopień studiów:", end=" ")
-if stopien == "a":
-    print("Studia inżynierskie")
-elif stopien == "b":
-    print("Studia magisterskie")
-else:
-    print("Niepoprawna odpowiedź")
-
-print("6. Sposób finansowania studiów:", end=" ")
-if finansowanie == "a":
-    print("Studia zaoczne")
-elif finansowanie == "b":
-    print("Studia dzienne, płatne")
-else:
-    print("Inne: ", finansowanie)
-
-print("7. Planowana specjalizacja po ukończeniu studiów:", end=" ")
 if specjalizacja == "a":
-    print("Programowanie aplikacji biznesowych")
+    answers += ["Programowanie aplikacji biznesowych"]
 elif specjalizacja == "b":
-    print("PM lub BA")
+    answers += ["PM lub BA"]
 elif specjalizacja == "c":
-    print("3D artist")
+    answers += ["3D artist"]
 else:
-    print("Inne: ", specjalizacja)
+    answers += [f"Inne: {specjalizacja}"]
+
+print(f"\nPodsumowanie odpowiedzi dla {imie_nazwisko}:")
+
+for x in range(7):
+    print("Pytanie: ", questions[x])
+    print("Odpowiedz: ", answers[x])
